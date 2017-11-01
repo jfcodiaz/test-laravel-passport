af9af84c8821cc7b17266fe17043f496fec06b56
José Francisco Díaz <jfcodiaz@gmail.com>
2017-11-01 13:46:11 -0600

    Prueba con Client Credentials Grant Tokens

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,18 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('posts', function (){
+Route::get('posts', function () {
     return App\Post::all();
 })->middleware('auth:api');
 
+Route::get('clients/posts', function() {
+    return App\Post::all();
+})->middleware('client');
+
+Route::post('clients/posts', function(Request $request) {
+    App\Post::create([
+        'title' => $request->input('title'),
+        'body' => $request->input('body')
+    ]);
+    return ['status' => 200];
+})->middleware('client');
